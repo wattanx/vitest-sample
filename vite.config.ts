61c61a20ab89bb4759771bce0d7d06f2cea5dc84
wattanx <wattan.dev@gmail.com>
2022-02-18 23:22:46 +0900

    fix: setting Vitest

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,8 @@ import { createVuePlugin } from "vite-plugin-vue2";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [createVuePlugin()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+  },
 });
