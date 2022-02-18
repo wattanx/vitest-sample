2137200eec601d42236d2b70c82364b8982958f5
wattanx <wattan.dev@gmail.com>
2022-02-18 23:22:57 +0900

    fix: add vitest

--- src/test/Center.spec.ts
+++ src/test/Center.spec.ts
@@ -2,12 +2,23 @@ import { mount } from "@vue/test-utils";
 import Center from "../components/Center.vue";
 
 describe("Center Test", () => {
-  it("slotの内容が描画されること", () => {
+  it("slotの内容が描画されること_Jestでは通る", () => {
     const wrapper = mount(Center, {
       slots: {
         text: "<p>test</p>",
       },
     });
+    console.log(wrapper.html());
     expect(wrapper.html()).toContain("<p>test</p>");
+  });
+
+  it("slotの内容が描画されること_Jest・Vitest両方で通る", () => {
+    const wrapper = mount(Center, {
+      slots: {
+        text: "<p>test</p>",
+      },
+    });
+    console.log(wrapper.html());
+    expect(wrapper.find("p").text()).toBe("test");
   });
 });
